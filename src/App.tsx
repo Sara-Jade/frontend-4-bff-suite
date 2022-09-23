6e497ba03f8fc596eeab3ea848c7d4ef3fb5574b
SuperJuicer <Sara.Jade@alaskaair.com>
2022-09-22 20:02:00 -0700

    Added TodoForm. App now can complete, delete, and add a todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Loader from './Components/Loader';
+import TodoForm from './Components/TodoForm';
 import Todo from './Todo';
 import { TodoType } from './Types/Todo.type';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<TodoType[]>([]);
+  const [error, setError] = useState({});
+
+  const toggleDone = (index: number) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const handleOnSubmit = (inputValue: string) => {
+    setTodos(
+      [...todos, {
+        title: inputValue,
+        id: todos.length + 1,
+        completed: false
+      }]
+    )
+  };
 
   useEffect(() => {
     fetch('http://jsonplaceholder.typicode.com/todos')
     .then(res => res.json())
     .then(res => setTodos(res.slice(0, 10)))
-    .catch(err => console.log(err));
+    .catch(err => setError(err));
   }, []);
 
   return (
     <div className="App">
       {todos.length > 0 ?
-        todos.map((todo: TodoType) => <Todo todo={todo} />) :
+        todos.map((todo: TodoType, index) => <Todo todo={todo} index={index} toggleDone={toggleDone} deleteTodo={deleteTodo}/>) :
         <Loader/>}
+      <TodoForm handleOnSubmit={handleOnSubmit}/>
     </div>
   );
 }
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -3,10 +3,19 @@ import { TodoType } from './Types/Todo.type';
 
 interface Props {
     todo: TodoType;
+    index: number;
+    toggleDone: (id: number) => void;
+    deleteTodo: (id: number) => void;
 }
 
-const Todo: React.FC<Props> = ({todo}) => {
-    return (<>Todo...</>)
+const Todo: React.FC<Props> = ({todo, index, toggleDone, deleteTodo}) => {
+    return (<div className='todo' style={{textDecoration: todo.completed ? 'line-through' : ''}}>
+        {todo.title}
+        <div>
+            <button onClick={() => toggleDone(index)}>{todo.completed ? 'Done' : 'Todo'}</button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+        </div>
+    </div>)
 }
 
 export default Todo;
